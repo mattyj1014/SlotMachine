04beab570ca5af38847e891cc962174e4cbcee0a
Matthew Jenks <matthewjenks@Matthews-MacBook-Pro.local>
2014-09-20 13:59:48 -0700

    first v

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Matthew Jenks on 9/18/14.
+//  Copyright (c) 2014 FiveWest. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image: UIImage = UIImage(named: "Ave")
+    var isRed = true
+}
